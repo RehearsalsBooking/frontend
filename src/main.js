32a76095b1d7947d085e1e985e6db86792060f88
belamov <business@belamov.com>
2020-04-18 21:25:31 +0500

    added basic layout. closes #1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
-axios.defaults.baseURL = 'http://localhost:8000/'
-Vue.use(VueAxios, axios)
+axios.defaults.baseURL = "http://localhost:8000/";
+Vue.use(VueAxios, axios);
 
 new Vue({
   vuetify,
